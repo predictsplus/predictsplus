ef824108dd8d36990e362cfce964e90c1b6f4f36
piyush2053 <piyushpatel2053@gmail.com>
2025-08-06 21:09:25 +0530

    feat [PP]: Center FooterNav icons and text

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -10,28 +10,31 @@ const FooterNav = () => {
     <footer className="fixed bottom-0 w-full mx-auto bg-[#1a1a1a] p-2 flex justify-around rounded-t-xl text-white z-50">
       <span
         onClick={() => navigate("/")}
-        className={`text-xl cursor-pointer flex-col justify-center ${location.pathname === "/" ? "text-pBlue" : ""}`}
+        className={`flex flex-col items-center justify-center gap-1 text-xl cursor-pointer ${location.pathname === "/" ? "text-pBlue" : ""}`}
       >
         <IoMdHome />
         <p className="text-xs">Home</p>
       </span>
+
       <span
         onClick={() => navigate("/live")}
-        className={`text-xl cursor-pointer ${location.pathname === "/live" ? "text-pBlue" : ""}`}
+        className={`flex flex-col items-center justify-center gap-1 text-xl cursor-pointer ${location.pathname === "/live" ? "text-pBlue" : ""}`}
       >
         <FaSuitcase />
         <p className="text-xs">My Bets</p>
       </span>
+
       <span
         onClick={() => navigate("/casino")}
-        className={`text-xl cursor-pointer ${location.pathname === "/casino" ? "text-pBlue" : ""}`}
+        className={`flex flex-col items-center justify-center gap-1 text-xl cursor-pointer ${location.pathname === "/casino" ? "text-pBlue" : ""}`}
       >
         <FaDice />
         <p className="text-xs">Casino</p>
       </span>
+
       <span
         onClick={() => navigate("/profile")}
-        className={`text-xl cursor-pointer ${location.pathname === "/profile" ? "text-pBlue" : ""}`}
+        className={`flex flex-col items-center justify-center gap-1 text-xl cursor-pointer ${location.pathname === "/profile" ? "text-pBlue" : ""}`}
       >
         <FaUser />
         <p className="text-xs">Profile</p>
